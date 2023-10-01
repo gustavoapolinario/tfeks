4a2c14064bf62ed0bafd921c914f565cb0c5c092
Gustavo Apolinario <gustavo.guss@gmail.com>
2023-10-01 20:39:07 -0300

    TF backend to save tfstate on S3 and the lock on dynamodb

---
+++ tf-backend-outputs.tf
@@ -0,0 +1,39 @@
+output "s3_bucket_domain_name" {
+  value       = module.terraform_state_backend.s3_bucket_domain_name
+  description = "S3 bucket domain name"
+}
+
+output "s3_bucket_id" {
+  value       = module.terraform_state_backend.s3_bucket_id
+  description = "S3 bucket ID"
+}
+
+output "s3_bucket_arn" {
+  value       = module.terraform_state_backend.s3_bucket_arn
+  description = "S3 bucket ARN"
+}
+
+output "s3_replication_role_arn" {
+  value       = module.terraform_state_backend.s3_replication_role_arn
+  description = "The ARN of the IAM Role created for replication, if enabled."
+}
+
+output "dynamodb_table_name" {
+  value       = module.terraform_state_backend.dynamodb_table_name
+  description = "DynamoDB table name"
+}
+
+output "dynamodb_table_id" {
+  value       = module.terraform_state_backend.dynamodb_table_id
+  description = "DynamoDB table ID"
+}
+
+output "dynamodb_table_arn" {
+  value       = module.terraform_state_backend.dynamodb_table_arn
+  description = "DynamoDB table ARN"
+}
+
+output "terraform_backend_config" {
+  value       = module.terraform_state_backend.terraform_backend_config
+  description = "Rendered Terraform backend config file"
+}
--- versions.tf
+++ versions.tf
@@ -20,10 +20,4 @@ terraform {
     }
   }
 
-  # ##  Used for end-to-end testing on project; update to suit your needs
-  # backend "s3" {
-  #   bucket = "terraform-ssp-github-actions-state"
-  #   region = "us-west-2"
-  #   key    = "e2e/karpenter/terraform.tfstate"
-  # }
 }
